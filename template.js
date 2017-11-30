976e61aec8b972f438b9aec8d5f827cb59dbc0f0
Aymeric <aymeric@kodono.info>
2017-11-30 14:25:54 +0100

    v2.0

--- template.js
+++ template.js
@@ -48,3 +48,7 @@ exports.init=function(configuration, plugins) {
   })
 }
 
+/**
+ * À noter qu'il est également possible de sauvegarder des informations supplémentaires dans le fichier configuration.json général
+ * Pour cela on appellera this.plugins.assistant.saveConfig('nom-du-plugin', {configuration_en_json_complète}); (exemple dans le plugin freebox)
+ */
