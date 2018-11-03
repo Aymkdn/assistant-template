7b0c6e8f1adf85b7bd570ad3832778b41d8509c0
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-11-03 10:57:50 +0100

    ES6 & function + prototypes => class

--- template.js
+++ template.js
@@ -1,37 +1,39 @@
-var request = require('request-promise-native'); // si vous souhaitez faire des requêtes HTTP
+const request = require('axios') // si vous souhaitez faire des requêtes HTTP
 
-/**
- * on crée une fonction `AssistantTemplate`
- * @param {Object} configuration L'objet `configuration` qui vient du fichier configuration.json
- */
-var AssistantTemplate = function(configuration) {
-  // par exemple configuration.key si on a `{ "key": "XXX" }` dans le fichier configuration.json
-  // exemple: this.key = configuration.key;
+class AssistantTemplate {
+  /**
+   * on crée un constructeur pour la classe `AssistantTemplate`
+   * @param {Object} configuration L'objet `configuration` qui vient du fichier configuration.json
+   */
+  constructor(configuration) {
+    // par exemple configuration.key si on a `{ 'key': 'XXX' }` dans le fichier configuration.json
+    // exemple: this.key = configuration.key
+  }
+
+  /**
+   * Il faut ensuite créer une méthode `init()`
+   *
+   * @param  {Object} plugins Un objet représentant les autres plugins chargés
+   * @return {Promise}
+   */
+  init(plugins) {
+    this.plugins = plugins
+    // si une configuration est requise (en reprenant l'exemple de 'key') :
+    // if (!this.key) return Promise.reject('[assistant-template] Erreur : vous devez configurer ce plugin !')
+    return Promise.resolve(this)
+  }
+
+  /**
+   * Méthode appelée par le système central
+   *
+   * @param {String} commande La commande envoyée depuis IFTTT par Pushbullet
+   * @return {Promise}
+   */
+  action(commande) {
+    // faire quelque chose avec `commande`
+    // votre code sera ici principalement
+  }
 }
-
-/**
- * Il faut ensuite créer une fonction `init()`
- *
- * @param  {Object} plugins Un objet représentant les autres plugins chargés
- * @return {Promise}
- */
-AssistantTemplate.prototype.init = function(plugins) {
-  this.plugins = plugins;
-  // si une configuration est requise (en reprenant l'exemple de "key") :
-  // if (!this.key) return Promise.reject("[assistant-template] Erreur : vous devez configurer ce plugin !");
-  return Promise.resolve(this);
-};
-
-/**
- * Fonction appelée par le système central
- *
- * @param {String} commande La commande envoyée depuis IFTTT par Pushbullet
- * @return {Promise}
- */
-AssistantTemplate.prototype.action = function(commande) {
-  // faire quelque chose avec `commande`
-  // votre code sera ici principalement
-};
 
 /**
  * Initialisation du plugin
@@ -40,15 +42,15 @@ AssistantTemplate.prototype.action = function(commande) {
  * @param  {Object} plugins Un objet qui contient tous les plugins chargés
  * @return {Promise} resolve(this)
  */
-exports.init=function(configuration, plugins) {
+exports.init = (configuration, plugins) => {
   return new AssistantTemplate(configuration).init(plugins)
-  .then(function(resource) {
-    console.log("[assistant-template] Plugin chargé et prêt.");
-    return resource;
-  })
+    .then(resource => {
+      console.log('[assistant-template] Plugin chargé et prêt.')
+      return resource
+    })
 }
 
 /**
  * À noter qu'il est également possible de sauvegarder des informations supplémentaires dans le fichier configuration.json général
- * Pour cela on appellera this.plugins.assistant.saveConfig('nom-du-plugin', {configuration_en_json_complète}); (exemple dans le plugin freebox)
+ * Pour cela on appellera this.plugins.assistant.saveConfig('nom-du-plugin', {configuration_en_json_complète}) (exemple dans le plugin freebox)
  */
